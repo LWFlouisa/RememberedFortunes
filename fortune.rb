4e44832dc6166d8c0daf37a318c10acd1fd81f7c
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-06-09 21:52:47 -0500

    Update fortune.rb

--- fortune.rb
+++ fortune.rb
@@ -1,5 +1,5 @@
 def fortune_generation
-  iteration = Fiile.read("_ai/gen_limit/limitation.txt").strip.to_i
+  iteration = File.read("_ai/gen_limit/limitation.txt").strip.to_i
 
   iteration.times do
     # Make fortunes cookies by two future array clones.
@@ -7,29 +7,32 @@ def fortune_generation
     fortune_cookie2 = File.readlines("_ai/fortunes/set_two/futures.txt")
 
     # Use sampler for each fortune cookie
-    first_outcome  = fortune_cookie1.sample.strip
-    second_outcome = fortune_cookie2.sample.strip
-    outcome_name   = "#{first_outcome}_#{second_outcome}".tr " ", "_"
+    first_fortune  = fortune_cookie1.sample.strip
+    second_fortune = fortune_cookie2.sample.strip
+    outcome_name   = "result".tr " ", "_"
 
-    if first_outcome == second_outcome
-      puts "First fortune: #{first_outcome} Second fortune: #{second_outcome}"
+    if first_fortune == second_fortune
+      puts "First fortune: #{first_fortune} Second fortune: #{second_fortune}"
 
       puts ">> These outcomes are similar and thus remembering these for later..."
 
       ## Create a new document based on remembered datapoint
       open("_outcomes/remembered_futures/fortune.txt", "a") { |f|
-        f.puts first_outcome
+        f.puts first_fortune
       }
 
       ## Create a new prolog knowledge base section upon appending.
       open("_knowledgebase/#{outcome_name}.pl", "a") { |f|
-        f.puts outcome_name
+        f.puts first_fortune
       }
+
+      abort
     else
-      puts "First fortune: #{first_outcome} Second fortune: #{second_outcome}"
+      puts "First fortune: #{first_fortune} Second fortune: #{second_fortune}"
 
       puts ">> These outcomes are not similar and thus must be unconnected..." 
     end
   end
+end
   
-  fortune_generation
+fortune_generation
